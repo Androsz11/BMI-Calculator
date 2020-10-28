eec89e8b4bd7fb4bbfa675daa18db3caa606940c
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2020-10-28 14:46:15 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,14 @@
+print("BMI Calculator")
+print
+print("Scripted by Hritabhas Modak")
+print
+print
+Weight=int(input("Enter weight in KG"));
+Height=int(input("Enter height in Centimeter"));
+BMI=Weight/Height**2*10000;
+print("Your BMI is",BMI,".");
+print
+print("Below 18.5","-","Underweight BMI");
+print("18.5","-","24.9","-","Normal BMI");
+print("25","-","29.9","-","Overweight BMI");
+print("30 and above","-","Obese BMI");
