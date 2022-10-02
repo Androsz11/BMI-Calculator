3ce885ef1428d5f768905d90ed56fbfa2748c7a7
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2022-10-02 12:24:21 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,14 +1,25 @@
-print("BMI Calculator")
-print
-print("Scripted by HMAndro")
-print
-print
-Weight=float(input("Enter weight in KG"));
-Height=float(input("Enter height in Centimeter"));
+print("BMI Calculator\n")
+print("Scripted by HMAndro\n\n")
+
+Weight=float(input("Enter weight in KG: "));
+Height=float(input("Enter height in Centimeter: "));
+
 BMI=Weight/Height**2*10000;
 print("Your BMI is",BMI,".");
-print
-print("Below 18.5","-","Underweight BMI");
-print("18.5","-","24.9","-","Normal BMI");
-print("25","-","29.9","-","Overweight BMI");
-print("30 and above","-","Obese BMI");
+
+if (BMI <= 18.5):
+    print("Underweight BMI",BMI)
+    
+elif (BMI >= 18.6 and BMI <=24.9):
+    print("Normal BMI",BMI)
+    
+elif (BMI >= 25 and BMI <=29.9):
+    print("Overweight BMI",BMI)
+    
+elif (BMI >= 30):
+    print("Obese BMI",BMI)
+# print
+# print("Below 18.5","-","Underweight BMI");
+# print("18.5","-","24.9","-","Normal BMI");
+# print("25","-","29.9","-","Overweight BMI");
+# print("30 and above","-","Obese BMI");
